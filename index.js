51fc433b19faeccac1adc3b549f3cfedadb4a5d0
Alex <aminyakov94@gmail.com>
2023-06-22 12:39:38 -0500

    added inquirer question

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const SVG = require ("./lib/svg")
 function promtUser(){
     inquirer.prompt([
 //create inquirer questions
+{
+    type: "input",
+    name: "text",
+    message: "Please enter text for logo. (Text should be up to be 3 characters)",
+},
+{
+    type: "input",
+    name: "textcolor",
+    message: "Please enter a text color (Enter keyword or a hexadecimal number):",
+},
+{
+
+    type: "input",
+    name: "textbackgroundColor",
+    message: "Please enter shape color (Enter keyword (or a hexadecimal number):",
+},
+{
+    type: "list",
+    name: "shape",
+    message: "Please choose which shape you would like?",
+    choices: ["Circle", "Square", "Triangle"],
+},
     ]).then((answers)=>{
 //use a switch case for the differnt shapes
 //set the logo color and the text color
